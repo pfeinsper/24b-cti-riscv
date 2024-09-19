1feacc80bacf55cc88d5d41ab978398a5c2aa518
RodrigoAnciaes <81170574+RodrigoAnciaes@users.noreply.github.com>
2024-09-19 16:21:42 -0300

    Basic function and import structure.

---
+++ de0-nano-sdram-qsys/sw/example/adc_helper/include/adc.h
@@ -0,0 +1,19 @@
+
+
+#ifndef ADC_H
+#define ADC_H
+
+#include <stdint.h>
+
+
+//
+// Function prototypes
+//
+
+void adc_start(void);
+
+void select_chanel(uint32_t chanel);
+
+uint32_t adc_read(void);
+
+#endif
